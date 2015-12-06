1ed0150e4d32c099af817e6eb4b189661dbb17f3
waynechen <wjmuse@gmail.com>
2015-12-06 20:54:36 +0800

    Change MQTT Topic

--- sbin/send-logfile.py
+++ sbin/send-logfile.py
@@ -5,27 +5,27 @@ import json
 import sys
 import os.path
 import argparse
+#import request
 from datetime import datetime
-from elasticsearch import Elasticsearch
+from elasticsearch import Elasticsearch, helpers
 from elasticsearch_dsl import DocType, String, Boolean
 from kafka import SimpleProducer, KafkaClient
 from elasticsearch_dsl.connections import connections
 connections.create_connection(hosts=['52.74.47.196', ])
 
 INDEX_PREFIX = "analysis-"
+ES_CLUSTER = 'http://localhost:9200/'
+ES_INDEX = 'test'
+ES_TYPE = 'log'
 
 
 class ESconnection:
     def __init__(self):
-        self.json_datas=[]
-        es = Elasticsearch()
+        # self.json_datas=[]
+        self.es = Elasticsearch(ES_CLUSTER)
 
         # es.index(index="my-index", doc_type="test-type", id=42, body={"any": "data", "timestamp": datetime.now()})
         # {u'_id': u'42', u'_index': u'my-index', u'_type': u'test-type', u'_version': 1, u'ok': True}
-
-        # es.index(index="social", doc_type="tweet", id=2,
-        # body={"content": "大家好2","user":{"name":"小王","id":670086},
-        # "tags":["demo","test2"], "timestamp": datetime.now()})
 
         # es.index(index='posts', doc_type='blog', id=3, body={
         # 'author': 'Benjamin Pollack',
@@ -34,10 +34,107 @@ class ESconnection:
         # 'topics': ['mercurial', 'git', 'flamewars', 'hidden messages'],
         # 'awesomeness': 0.95
 
-    def send_to_es():
-        #es = Elasticsearch()
-        pass
+    def send_to_es(self, index_name, json_docs):
+        #es = Elasticsearch(index=index_name, body={'refresh_interval': '5s'})
+        # self.es.indices.create()
+        # self.es.bulk(json_docs, index_name, ES_TYPE, raise_on_error=True)
 
+        """
+        entry_mapping = {
+            'entry-type': {
+                'properties': {
+                    'id': {'type': 'integer'},
+                    'published': {'type': 'date'},
+                    'title': {'type': 'string'},
+                    'tags': {'type': 'string', 'analyzer': 'keyword'},
+                    'content': {'type': 'string'}
+                }
+            }
+        }
+        """
+        """
+        es.indices.create(
+        index='blog-index',
+        body={
+            'mappings': {
+                'blog-entry-type': {
+                    'properties': {
+                        'id': {'type': 'integer'},
+                        'title': {'type': 'string'},
+                        'content': {'type': 'string'},
+                        'tags': {'type': 'string'},
+                        'created': {'type': 'date'}
+                    }
+                }
+            }
+        }
+        """
+        if self.es.indices.exists(index_name):
+            print "deleting '%s' index..." % (index_name)
+            print(self.es.indices.delete(index= index_name, ignore=[400, 404]))
+            #self.es.indices.delete(index_name)
+
+
+        self.es.indices.create(
+            index=index_name,
+            body={
+                'mappings': {
+                    'sensor': {
+                        'properties': {
+                            'date': {'type': 'date', "format": "strict_date_optional_time||epoch_millis"},
+                            'datetime': {'type': 'date', "format": "strict_date_optional_time||epoch_millis"},
+                            'location': {'type': 'geo_point'}
+                        }
+                    }
+                }
+            }
+        )
+
+        #self.es.indices.create(index_name)
+
+        #self.es.health(wait_for_status='yellow')
+        #self.es.create(index=index_name, settings={'mappings': entry_mapping})
+
+        print "TO_ES %s %d" % (index_name, len(json_docs))
+        """
+        aa=self.es.index(index='posts', doc_type='blog', id=1, body={
+            'author': 'Santa Clause',
+            'blog': 'Slave Based Shippers of the North',
+            'title': 'Using Celery for distributing gift dispatch',
+            'topics': ['slave labor', 'elves', 'python',
+                       'celery', 'antigravity reindeer'],
+            'awesomeness': 0.2
+        })
+        """
+
+        actions = []
+        '''
+        for doc in json_docs:
+            action = {
+                "_index": index_name,
+                "_type": "test",
+                "_id": doc_index + 1,
+                "_source": json_docs[doc]
+            }
+            doc_index = doc_index + 1
+        '''
+        for idx, doc in enumerate(json_docs):
+            action = dict(_index=index_name, _type="test", _id=idx + 1, _source=json_docs[idx])
+            actions.append(action)
+            #actions.append(action)
+
+
+        if len(actions) > 0:
+            #self.es.bulk(index=index_name, body=actions)
+            helpers.bulk(self.es, actions)
+
+        #self.es.bulk(index,index_name,"test",actions)
+        #def bulk(self, body, index=None, doc_type=None, params=None):
+
+        # self.es.bulk()
+        # self.es.index(index=index_name, doc_type='test', id=1, body=json_docs[1])
+        #print aa
+        # self.es.delete()
 
 class KFconnection:
     def __init__(self):
@@ -80,6 +177,14 @@ class Datas:
             col_pair = element.split("=")
             if len(col_pair) >= 2:
                 self.value_dict[col_pair[0]] = col_pair[1]
+
+        # [value for key, value in programs.items() if 'new york' in key.lower()]:
+        # [float(self.value_dict(k)) for (k, v) in self.value_dict.iteritems() if 's_' in k]
+        # for (k, v) in self.value_dict.iteritems() if 's_' in k:
+        for (k, v) in self.value_dict.iteritems():
+            if k.startswith('s_'):
+                self.value_dict[k] = float(self.value_dict[k])
+
         if all (key in self.value_dict for key in ("gps_lat","gps_lon","gps_alt","date","time")):
             # print self.value_dict.keys()
             gps_lat = self.value_dict["gps_lat"]
@@ -90,10 +195,14 @@ class Datas:
             self.value_dict["gps_lat"] = self.gps_to_map(float(gps_lat))
             self.value_dict["gps_lon"] = self.gps_to_map(float(gps_lon))
             self.value_dict["gps_alt"] = self.gps_to_map(float(gps_alt))
+            self.value_dict["gps_fix"] = int(self.value_dict["gps_fix"])
+            # self.value_dict["datetime"] = str( datetime.now() )
 
-            self.value_dict["datetime"] = datetime.datetime.strptime(
-            self.value_dict["date"] + " " + self.value_dict["time"], '%Y-%m-%d %H:%M:%S')
-            # combine object
+            self.value_dict["datetime"] = datetime.strptime(self.value_dict["date"] + " " +
+                                                            self.value_dict["time"], '%Y-%m-%d %H:%M:%S').isoformat()
+            # "yyyy-MM-dd'T'HH:mm:ssZ"
+            # self.value_dict["datetime"] = datetime.fromtimestamp(record_time).strftime('%Y-%m-%d %H:%M:%S')
+            # self.value_dict["datetime"] = record_time.isoformat()
             return self.value_dict
         else:
             return None
@@ -120,6 +229,7 @@ def main():
     global esData
 
     esData = Datas()
+    esSender = ESconnection()
 
     parser.add_argument('-l','--logfile', metavar=("logfile"),help="input lass logfile with directory")
     parser.add_argument('-i','--index', metavar=("logfile"),default=INDEX_PREFIX,help="ElasticSearch index prefix")
@@ -130,10 +240,10 @@ def main():
 
     if args.logfile is not None:
         if os.path.isfile(args.logfile):
-            datetime = args.logfile.split('-')
+            index_date = args.logfile.split('-')
             #print "qq  %s" % datetime[1]
             load_raw(args.logfile)
-            #send_to_es(INDEX_PREFIX + datetime[1])
+            esSender.send_to_es(INDEX_PREFIX + index_date[1], esData.json_datas)
             #print esData.json_datas
         else:
             print "Please specify a correct filename."
