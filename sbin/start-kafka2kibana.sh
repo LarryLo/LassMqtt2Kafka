ec460428fb40a95f68fa127aef0bacefd4177cf3
LarryLo <larrylo@etusolution.com>
2016-04-12 19:51:54 +0800

    Add geo_mapping before start importing data into elastic search and change mqtt topic

--- sbin/start-kafka2kibana.sh
+++ sbin/start-kafka2kibana.sh
@@ -1,1 +1,2 @@
+curl -X PUT -d '{"mappings":{"basic":{"properties":{"location":{"type":"geo_point"}}}}}' localhost:9200/lass-geo
 nohup /opt/spark/bin/spark-submit --total-executor-cores 2 --executor-memory 2G ~/Lass.jar LASSxKibana > /var/log/lass-kibana.log 2>&1 &
